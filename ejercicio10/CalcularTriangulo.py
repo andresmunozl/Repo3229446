39643598a2a082b2a9265b215a6317ec347fe003
Juan Jaller <juancabrera1293@gmail.com>
2026-07-30 14:23:18 -0500

    feat: agregar solución del ejercicio CalcularTriangulo.py

--- ejercicio10/CalcularTriangulo.py
+++ ejercicio10/CalcularTriangulo.py
@@ -1,1 +1,7 @@
 ## Clase CalculadoraTriangulo:  Atributos base y altura. Método para calcularArea() ().
+base = float(input("base triangulo: "))
+altura = float(input("altura triangulo: "))
+
+calcularArea = base * altura / 2
+
+print("El area es: ", calcularArea)
