effb68ebe035307075bd96265dad357857a13a65
Jose Jose <eope820529@gmail.com>
2026-07-31 15:03:19 -0500

    Se crea la clase ConversorTemperatura

--- ejercicio3/ConversorTemperatura.py
+++ ejercicio3/ConversorTemperatura.py
@@ -1,1 +1,15 @@
 ## Clase ConversorTemperatura:  Atributo celsius. Métodos para aFahrenheit() y aKelvin(). 
+class ConversorTemperatura:
+    def __init__(self, celsius):
+        self.celsius = celsius
+
+    def a_fahrenheit(self):
+            return (self.celsius * 9/5) + 32
+
+    def a_kelvin(self):
+            return self.celsius + 273.15
+
+mi_temp = ConversorTemperatura(25)
+
+print("Temperatura en Fahrenheit: ", mi_temp.a_fahrenheit())
+print("Temperatura en Kelvin: ", mi_temp.a_kelvin())
