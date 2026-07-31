6ff3a5c31876ebf85069f9c250e5721821188ec1
Cristian Meneses <cristian.meneses.asesor.mpal@gmail.com>
2026-07-31 17:52:43 -0500

    Ejercicios 1 y 12: CalculadoraBasica y CalculadoraIMC
    - Ejercicio 1: CalculadoraBasica con metodos sumar() y restar()
    - Ejercicio 12: CalculadoraIMC con atributos peso/altura y metodo obtenerIndice()
    - Cristian Meneses - Ficha 3229446

--- ejercicio12/CalculadoraIMC.py
+++ ejercicio12/CalculadoraIMC.py
@@ -1,1 +1,7 @@
-##  Clase CalculadoraIMC: Atributos peso y altura. Método para obtenerIndice() (). 
+class CalculadoraIMC:
+    def __init__(self, peso, altura):
+        self.peso = peso
+        self.altura = altura
+
+    def obtenerIndice(self):
+        return round(self.peso / (self.altura ** 2), 2)
