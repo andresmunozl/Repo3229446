b666b6691d386ff31fb76273ffdcb2053dfceafe
Cristian <lubocristianandres@gmail.com>
2026-07-30 22:18:25 -0500

    feat: Realize ejercicio 7 y 14

--- ejercicio14/CalculadoraVelocidad.py
+++ ejercicio14/CalculadoraVelocidad.py
@@ -1,1 +1,7 @@
 ## Clase CalculadoraVelocidad: Atributos distancia y tiempo. Método para calcularVelocidadMedia().. 
+class CalculadoraVelocidad:
+
+    def calcularVelocidadMedia(distancia, tiempo) :
+        return distancia / tiempo
+
+print(CalculadoraVelocidad.calcularVelocidadMedia(100,2))
