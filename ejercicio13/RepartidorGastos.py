e9947ca7d4dde7709533073f106ee0d82a59223c
andres <es@example.com>
2026-07-31 20:09:42 -0500

    feat:ejercicio 13

--- ejercicio13/RepartidorGastos.py
+++ ejercicio13/RepartidorGastos.py
@@ -1,1 +1,17 @@
 ##  Clase RepartidorGastos: Atributos totalFactura y numeroPersonas. Método para divisionEquitativa(). 
+class RepartidorGastos:
+    
+    def __init__(self, totalFactura, numeroPersonas):
+        self.totalFactura = totalFactura
+        self.numeroPersonas = numeroPersonas
+    
+    def divisionEquitativa(self):
+        return self.totalFactura / self.numeroPersonas
+
+
+# Programa principal con valores fijos
+reparto = RepartidorGastos(100, 4)
+
+print("Total factura:", reparto.totalFactura)
+print("Número de personas:", reparto.numeroPersonas)
+print("Cada persona paga:", reparto.divisionEquitativa())
