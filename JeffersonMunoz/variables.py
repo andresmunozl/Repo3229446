7a2a10bacd4dd7edee6125cd8a432aa2d1964d67
Andres M <amunoz@octapus.io>
2026-08-05 21:16:38 -0500

    feat: ejercicio variables hecho

---
+++ JeffersonMunoz/variables.py
@@ -0,0 +1,20 @@
+print('Hola Mundo / Hello World')
+
+nombre_completo = 'Jefferson Muñoz'
+print(nombre_completo)
+
+numero1 = 1
+numero2 = 1
+
+print(numero1 + numero2)
+
+es_profesor = True
+es_aprendiz = False
+
+print(es_profesor)
+
+apellidos_completos: str = 'Munoz Luligo'
+
+print(apellidos_completos)
+
+print(type(es_profesor))
