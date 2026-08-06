b3a4436d5576c28602d50eb4c7d1187341b1e3a3
AndresL2525 <andresluna1520@gmail.com>
2026-08-05 21:15:25 -0500

    fix trabajo Camilo Andres

---
+++ CamiloAndresLuna/Variables.py
@@ -0,0 +1,21 @@
+print("Hello word")
+
+Nombre_Completo = 'Andres'
+
+print(Nombre_Completo)
+
+numero1 = 1
+numero2 = 1
+print(numero1 + numero2)
+
+es_profesor = True
+es_sprendiz = False
+print(es_sprendiz)
+
+apellido_completo: str = 'Luna'
+print(apellido_completo) 
+
+es_martes = False
+es_miercoles = True
+print(es_miercoles)
+print(type(es_miercoles))
