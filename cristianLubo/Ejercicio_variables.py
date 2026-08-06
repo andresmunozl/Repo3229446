e8c5bbbf21511ad621ac0666b9d95065c8da2313
Cris <es@example.com>
2026-08-05 21:13:48 -0500

    feat: subo el ejercicio solicitado en clase

---
+++ cristianLubo/Ejercicio_variables.py
@@ -0,0 +1,26 @@
+# punto1
+nombre = input("Ingrese su nombre: ")
+
+edad = input("Ingrese su edad: ")
+
+altura = input("Ingrese su altura en metros: ")
+
+estudiante = input("Es estudiante (si o no): ")
+
+# punto2
+nombre = str(nombre)
+
+
+edad = int(edad)
+
+
+altura = float(altura)
+
+
+estudiante = bool(estudiante)
+
+
+print(f"nombre: {nombre}")
+print(f"edad: {edad}")
+print(f"altura: {altura:.2f}")
+print(f"estudiante: {estudiante}")
---
+++ cristianLubo/variables.py
@@ -0,0 +1,29 @@
+print('Hola mundo')
+
+nombre_completo = 'Cristian Lubo'
+print(nombre_completo)
+
+numero1 = 1
+numero2 = 1
+
+print(numero1 + numero2)
+
+es_profesor = True
+es_aprendiz = False
+
+print(es_profesor)
+
+apellidos_completos: str = 'Lubo Realpe'
+
+print(apellidos_completos)
+
+saldo: int = 5000
+
+print(saldo)
+
+es_nequi = False
+es_bancolombia = True
+
+print(es_bancolombia)
+
+print(type(es_bancolombia))
