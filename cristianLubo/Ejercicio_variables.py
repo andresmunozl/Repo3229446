ca1642d09404dbb5d2354ba24460d25d66c82240
Cristian <lubocristianandres@gmail.com>
2026-08-05 22:04:21 -0500

    feat: ajuste espacios en blanco

--- cristianLubo/Ejercicio_variables.py
+++ cristianLubo/Ejercicio_variables.py
@@ -1,25 +1,16 @@
 # punto1
 nombre = input("Ingrese su nombre: ")
-
 edad = input("Ingrese su edad: ")
-
 altura = input("Ingrese su altura en metros: ")
-
 estudiante = input("Es estudiante (si o no): ")
 
 # punto2
 nombre = str(nombre)
-
-
 edad = int(edad)
-
-
 altura = float(altura)
-
-
 estudiante = bool(estudiante)
 
-
+# punto3
 print(f"nombre: {nombre}")
 print(f"edad: {edad}")
 print(f"altura: {altura:.2f}")
--- cristianLubo/variables.py
+++ cristianLubo/variables.py
@@ -5,25 +5,19 @@ print(nombre_completo)
 
 numero1 = 1
 numero2 = 1
-
 print(numero1 + numero2)
 
 es_profesor = True
 es_aprendiz = False
-
 print(es_profesor)
 
 apellidos_completos: str = 'Lubo Realpe'
-
 print(apellidos_completos)
 
 saldo: int = 5000
-
 print(saldo)
 
 es_nequi = False
 es_bancolombia = True
-
 print(es_bancolombia)
-
 print(type(es_bancolombia))
