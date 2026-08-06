f5a5a35f4cc127c7b6183b1ae7965bc4357a2601
Cristian <casanovaartes@gmail.com>
2026-08-05 21:26:39 -0500

    feat:ejercicio_variable.py

---
+++ CristianMeneses/ejercicio_variables.py
@@ -0,0 +1,6 @@
+nombre = input("ingrese su nombre")
+edad = int(input("ingrese edad"))
+altura = float(input("ingrese altura"))
+estudiante = bool(input("ingrese el estudiante"))
+
+print(f"Nombre:{nombre},Edad:{edad},Altura:{altura},Estudiante:{estudiante}")
