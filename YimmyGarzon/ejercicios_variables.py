7ff1a4fdad00d10ab8765783e58ce081d1bf2f07
Jimgarcs <jimgarcs.yg@gmail.com>
2026-08-05 22:32:23 -0500

    feat: agregar ejercicio de variables con entrada de datos

--- YimmyGarzon/ejercicios_variables.py
+++ YimmyGarzon/ejercicios_variables.py
@@ -1,12 +1,12 @@
 nombre = input("Ingrese su nombre: ")
 edad = int(input("Ingrese su edad: "))
-altura = float(input("Ingrese su altura en metros ejm: 1.70):"))
+altura = float(input("Ingrese su altura en metros ejm: 1.70): "))
 
-estudiante = input("¿Es estudiante? (SI o NO): ")
+estudiante = input("¿Es estudiante? (si o no): ")
 
-es_estudiante = estudiante.lower() == "SI"
+es_estudiante = estudiante.lower() == "si"
 
-print(f"\n--- Datos del Usuario ---")
+print("\n--- Datos del Usuario ---")
 print(f"Nombre: {nombre}")
 print(f"Edad: {edad} años")
 print(f"Altura: {altura:.2f} metros")
