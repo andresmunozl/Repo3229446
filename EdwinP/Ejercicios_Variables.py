b4a86ef2f422c71ab31b783208cfdf0929212afa
Jose Jose <eope820529@gmail.com>
2026-08-05 21:18:57 -0500

    feat:Ejercicio_Variables

---
+++ EdwinP/Ejercicios_Variables.py
@@ -0,0 +1,17 @@
+# Paso 1: Pedir los datos al usuario
+nombre = input("Nombre: ")
+edad = int(input("Edad: "))
+altura = float(input("Altura en metros (ej: 1.70): "))
+es_estudiante = input("¿Es estudiante? (Si/No): ")
+
+#paso 2 convertir datos
+es_estudiante = es_estudiante == "Si"
+
+# paso 3 Imprima todos los datos en pantalla 
+
+print(f"\n--- Datos del Usuario ---")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad} años")
+print(f"Altura: {altura:.2f} metros")
+print(f"Estudiante: {es_estudiante}")
+
---
+++ EdwinP/variables.py
@@ -0,0 +1,27 @@
+print("Hello, World!/hola mundo")
+nombre_completo = "Edwin Potosi"
+print(nombre_completo)
+
+numero1 = '1'
+numero2 = '1'
+
+print(numero1 +numero2)
+
+
+es_profesor = True
+es_aprendiz = False
+
+print(es_profesor)
+
+apellidos_completos: str = "Potosi Espada"
+print(apellidos_completos)
+
+edad: int = 44
+activo: bool = True
+
+print(edad)
+print(activo)
+
+
+
+
