5641f625af79df7845929fcafcbc8a21d8a4e8f4
Cristian <casanovaartes@gmail.com>
2026-08-05 22:10:34 -0500

    fix: correción en impresión de altura con dos decimales

--- CristianMeneses/ejercicio_variables.py
+++ CristianMeneses/ejercicio_variables.py
@@ -3,4 +3,4 @@ edad = int(input("ingrese edad"))
 altura = float(input("ingrese altura"))
 estudiante = bool(input("ingrese el estudiante"))
 
-print(f"Nombre:{nombre},Edad:{edad},Altura:{altura},Estudiante:{estudiante}")
+print(f"Nombre:{nombre},Edad:{edad},Altura:{altura:.2f},Estudiante:{estudiante}")
