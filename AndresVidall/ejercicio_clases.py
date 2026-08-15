4293623423783e1f2b6377356d7d7bd8f44962fc
Juan Perez <juanperez@gmail.com>
2026-08-14 20:48:45 -0500

    calse y ejercicio de datos en clases

---
+++ AndresVidall/ejercicio_clases.py
@@ -0,0 +1,18 @@
+cars = {
+    'nombre': 'Macqueen',
+    'color':  'rojo',
+    'prlicula': 'Cars',
+    'copas_piston': True,
+    'mejores_amigos': ['Two Mater', 'Mack'],
+    'personajes' : {
+        'novia' : 'Saly',
+        'Frase' : '"Soy más rápido que rápido.  Más rápido que rápido. ¡Soy un rayo!"'
+    }
+}
+
+# imprimir el dicionari
+print(type(cars))
+print((cars))
+
+#imprimir la posicio
+print(list(cars.values())[1])
