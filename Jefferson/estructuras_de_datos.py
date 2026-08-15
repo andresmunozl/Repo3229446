739295306bbc1f95ee655657bb5f63eb667d2f5e
Andres M <amunoz@octapus.io>
2026-08-14 20:31:45 -0500

    ejemplo instructor

---
+++ Jefferson/estructuras_de_datos.py
@@ -0,0 +1,11 @@
+donaciones = ['enlatados', 'granos', 'agua', 'ropa']
+print(type(donaciones))
+
+
+print(donaciones)
+donaciones.append('medicinas')
+print(donaciones)
+donaciones.append('medicinas')
+donaciones.sort(reverse=False)
+print(donaciones)
+print(donaciones.count('medicinas'))
