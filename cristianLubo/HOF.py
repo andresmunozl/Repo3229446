0fc3550b7f62d178e8bd3ee9121764180c3f380d
Cris <es@example.com>
2026-08-19 21:26:52 -0500

    feat: subo archivo HOF.py

---
+++ cristianLubo/HOF.py
@@ -0,0 +1,21 @@
+def saludar():
+    return "Hola"
+
+
+def ejecutar(funcion):
+    return funcion()
+
+
+print(ejecutar(saludar))
+
+
+def multiplicar_por_dos(numero):
+    return numero * 2
+
+
+def aplicar_operacion(funcion, valor):
+    return funcion(valor)
+
+
+resultado = aplicar_operacion(multiplicar_por_dos, 5)
+print(resultado)
