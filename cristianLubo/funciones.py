90c55f18d4ee1ee39752e7202031712fc21ce1c8
Cris <es@example.com>
2026-08-19 21:10:57 -0500

    feat: ejercicio_mini_ia

---
+++ cristianLubo/funciones.py
@@ -0,0 +1,19 @@
+def calcular_temperatura():
+    # bloque de codigo
+    print('entro a la función')
+
+
+print('la temperatura de los galpones es 32°C')
+
+
+calcular_temperatura()
+calcular_temperatura()
+
+
+def saludar_a_todos():
+    print('Hola, bienvenido!')
+
+
+calcular_temperatura()
+saludar_a_todos()
+calcular_temperatura()
---
+++ cristianLubo/funciones_parametros_retornos.py
@@ -0,0 +1,12 @@
+def convertir_farenheit_a_celsius(nombre, grados_f: int = 10000):
+    celsius = (grados_f - 32) / 1.8
+    return celsius
+
+
+# resultado = convertir_farenheit_a_celsius(100) + 10
+# print(resultado)
+# convertir_farenheit_a_celsius(70)
+# grados = 100
+grados = int(input('Ingrese los graods en farenheit'))
+print(convertir_farenheit_a_celsius(grados_f=10, nombre='Bartolo'))
+print(convertir_farenheit_a_celsius('Camilo es gay'))
