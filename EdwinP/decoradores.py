977a4d7919f01d4f978b066aea2f6b9b03abcee2
Jose Jose <eope820529@gmail.com>
2026-08-21 20:18:19 -0500

    feat:decoradores

---
+++ EdwinP/decoradores.py
@@ -0,0 +1,31 @@
+def multiplicar_por_dos (numero):
+    return numero * 2   
+
+
+def aplicar_operacion(funcion, valor):
+    return funcion(valor)
+
+
+resultado = aplicar_operacion(multiplicar_por_dos, 5)
+print(resultado)
+
+# // ahora vamos a ver un ejercicio con decoradores
+
+def require_auth(func):
+    def wrapper(user):
+        if user.lower() == "admin":
+            return func(user)
+        else:
+            return "Acceso denegado"
+
+    return wrapper
+
+
+def admin_dashboard(user):
+    return f"Bienvenido al panel, {user}"
+
+
+auth_view_dashboard = require_auth(admin_dashboard)
+
+print(auth_view_dashboard("Admin"))
+print(auth_view_dashboard("Invitado"))
